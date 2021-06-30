6728d7038e71963f8ed75568d193163a9ec930c0
Helena de Araújo <helenadearaujo98@gmail.com>
2021-06-29 23:51:26 -0300

    Envio de atividade

--- src/controller/colaboradorasController.js
+++ src/controller/colaboradorasController.js
@@ -1,19 +1,26 @@
 const colaboradoras = require("../models/colaboradoras")
-
+const SECRET = process.env.SECRET
 
 
 const getAll = (req, res) => {
-  console.log(req.url);
-    colaboradoras.find(function (err, colaboradoras){
-      res.status(200).send(colaboradoras)
-    })     
+  const authHeader = req.get('authorization');
+  const token = authHeader.split('')[1];
+  console.log('Meu header:', token);
+
+  if (!token) {
+    return res.status(401).send('erro no header');
+  }
+  
+  colaboradoras.find(function (err, colaboradoras) {
+    res.status(200).send(colaboradoras)
+  })
 };
 
 const postColaboradora = (req, res) => {
   console.log(req.body);
 
   let colaboradora = new colaboradoras(req.body);
-    colaboradora.save(function(err){
+  colaboradora.save(function (err) {
     if (err) res.status(500).send({ message: err.message })
 
     res.status(201).send(colaboradora.toJSON());
@@ -24,6 +31,6 @@ const postColaboradora = (req, res) => {
 
 
 module.exports = {
-    getAll,
-    postColaboradora,
+  getAll,
+  postColaboradora,
 }
